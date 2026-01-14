8eeb0383e92f68ca6e323e24d655d0fa56d3e196
jimothy <jimothy@yarnl.com>
2026-01-14 01:41:32 +0000

    Switch baseUrl for custom domain

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://titandrive.github.io',
+  url: 'https://yarnl.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/yarnl-docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
